18f3b09622ac0a66b1aeddab3c65cda1a40e5bc6
Matthew Janes <m.janes351@mybvc.ca>
2020-04-14 15:29:37 -0600

    no message

--- run-stack.sh
+++ run-stack.sh
@@ -5,7 +5,7 @@ set -x
 STACK_NAME=$1
 ALB_LISTENER_ARN=$2
 
-if ! aws cloudformation describe-stacks --region us-west-2 --stack-name $STACK_NAME 2>&1 > /dev/null
+if ! aws cloudformation describe-stacks --region us-east-1 --stack-name $STACK_NAME 2>&1 > /dev/null
 then
     finished_check=stack-create-complete
 else
@@ -13,15 +13,15 @@ else
 fi
 
 aws cloudformation deploy \
-    --region us-west-2 \
+    --region us-east-1 \
     --stack-name $STACK_NAME \
     --template-file service.yaml \
     --capabilities CAPABILITY_NAMED_IAM \
     --parameter-overrides \
-    "DockerImage=380968173119.dkr.ecr.us-west-2.amazonaws.com/example-webapp:$(git rev-parse HEAD)" \
-    "VPC=vpc-be29b3c6" \
-    "Subnet=subnet-260afd5e" \
+    "DockerImage=089778365617.dkr.ecr.us-east-1.amazonaws.com/example-webapp:$(git rev-parse HEAD)" \
+    "VPC=vpc-affe74c9" \
+    "Subnet=subnet-23e0d06a" \
     "Cluster=default" \
     "Listener=$ALB_LISTENER_ARN"
 
-aws cloudformation wait $finished_check --region us-west-2 --stack-name $STACK_NAME
+aws cloudformation wait $finished_check --region us-east-1 --stack-name $STACK_NAME
