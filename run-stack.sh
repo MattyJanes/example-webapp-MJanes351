a783cb8400512708bdd26faedfb628472bb0cd60
Matthew Janes <m.janes351@mybvc.ca>
2020-04-15 14:55:55 -0600

    no message

--- run-stack.sh
+++ run-stack.sh
@@ -5,7 +5,7 @@ set -x
 STACK_NAME=$1
 ALB_LISTENER_ARN=$2
 
-if ! aws cloudformation describe-stacks --region us-east-1 --stack-name $STACK_NAME 2>&1 > /dev/null
+if ! aws cloudformation describe-stacks --region us-west-2 --stack-name $STACK_NAME 2>&1 > /dev/null
 then
     finished_check=stack-create-complete
 else
@@ -24,4 +24,4 @@ aws cloudformation deploy \
     "Cluster=default" \
     "Listener=$ALB_LISTENER_ARN"
 
-aws cloudformation wait $finished_check --region us-east-1 --stack-name $STACK_NAME
+aws cloudformation wait $finished_check --region us-west-2 --stack-name $STACK_NAME
